009b07bddf8f45b72d0351721369ba2ffb6578cd
Nicolas Bigaouette <nbigaouette@gmail.com>
2012-02-03 19:55:21 -0500

    New function: Get_dx()

--- src/LookUpTable.hpp
+++ src/LookUpTable.hpp
@@ -60,6 +60,7 @@ class LookUpTable
 
     // **************************************************************
     int     Get_n()                     { return n;         }
+    Double  Get_dx()                    { return dx;        }
     Double  Get_inv_dx()                { return inv_dx;    }
     Double  Get_XMin()                  { return range_min; }
     Double  Get_XMax()                  { return range_max; }
